b7fa528b7ad5de998ae35e1d66fdaa9996771547
haram <rlagkfka9611@naver.com>
2026-01-04 20:12:15 +0900

    add led_switch.c to control LED with a switch

--- LED_ONOFF_1218/Core/Src/led_switch.c
+++ LED_ONOFF_1218/Core/Src/led_switch.c
@@ -5,16 +5,30 @@ int main(void){
 	// GPIOD 활성화
 	RCC -> AHB1ENR |= 0x00000008;
 	RCC -> AHB1ENR |= 0x00000004;
+
 	//PC6을 General-purpose output 모드로 설정
-	GPIOC -> MODER &= ~0x000000C0;
-	GPIOC -> MODER |= 0x00000040;
+	GPIOC -> MODER &= ~0x00003000; //clear 13,12 bit
+	GPIOC -> MODER |= 0x00000040; //PC6 output mode
 
 	//PC6 중간속도로 설정
-	GPIOC -> OSPEEDR &= ~0x000000C0;
-	GPIOC -> OSPEEDR |= 0x00000040;
+	GPIOC -> OSPEEDR &= ~0x00003000; //clear 13,12 bit
+	GPIOC -> OSPEEDR |= 0x00000040; //PC6 Medium speed
 
 	//PD4를 입력으로 설정(스위치)
-	GPIOD -> MODER &= ~0x00000300;
+	GPIOD -> MODER &= ~0x00000300; //bit 8,9번 clear
+
+	while(1)
+	{
+		//IDR 레지스터 읽기: bit4 확인
+		if((GPIOD->IDR & 0x00000010)==0){
+			// 스위치 눌림 -> LED ON
+			GPIOC ->ODR &= ~0x00000040; // clear bit6
+		}
+		else{
+			//스위치 떼짐 -> LED OFF
+			GPIOC -> ODR |= 0x00000040; // set bit 6
+		}
+	}
 
 
 }
